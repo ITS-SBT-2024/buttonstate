dae2ccbf2f70706c798e8e99df719e9bd5209589
Axel...97 <lelialex97@gmail.com>
2024-05-20 13:25:37 +0200

    app contatore

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [contatore, setContatore] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <button style={{
+      backgroundColor: "green",
+      borderRadius: "40px",
+      display: "flex",
+      justifyContent: "center",
+     }} onClick={() => setContatore(contatore + 1)}>
+      Sali di un piano!
+      </button>
+
+      <p style={{fontFamily:"Arial", margin:"40px", textAlign:'center'}}>
+        Ti trovi al piano numero <b>{contatore}</b>
       </p>
+
+      <button style={{
+      backgroundColor: "red",
+      borderRadius: "40px",
+      display: "flex",
+      justifyContent: "center"
+     }} onClick={() => setContatore(contatore - 1)}>
+      Scendi di un piano!
+      </button>
     </>
   )
 }
